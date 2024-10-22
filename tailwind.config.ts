67bd3b51a5d2311e07f720cc8f3be00ada38c33d
Kevin Rousseau <94388367+kevinrss01@users.noreply.github.com>
2024-10-22 13:19:59 +0200

    Add files via upload

---
+++ tailwind.config.ts
@@ -0,0 +1,36 @@
+import type { Config } from "tailwindcss";
+// tailwind.config.js
+import {nextui} from "@nextui-org/react";
+
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    // ...
+    // make sure it's pointing to the ROOT node_module
+    "./node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  darkMode: "class",
+  plugins: [nextui()],
+};
+
+const config: Config = {
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        background: "var(--background)",
+        foreground: "var(--foreground)",
+      },
+    },
+  },
+  plugins: [nextui()],
+};
+export default config;
